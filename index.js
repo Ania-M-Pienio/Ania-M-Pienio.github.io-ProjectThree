95481d2238aef2fbedaba6cda268ddf6387fb060
Ania-M-Pienio <aniapienio@gmail.com>
2020-02-07 10:01:51 -0500

    button text update functionality

--- index.js
+++ index.js
@@ -2,33 +2,55 @@ const app = {};
 
 // declares variables after doc is ready
 app.variables = function() {
-  app.$preview = $(`.preview.edit`);
+  app.$button = $(`.preview.edit button`);
 }
 
-
+// scrolls to the element with the given id
 app.scrollToElem = function(id) {
   let element = document.getElementById(id);
   element.scrollIntoView({ behavior: "smooth" });
 };
 
+// replaces the innter text node of the button
+app.replaceButtonText = function(text) {
+     app.$button.text(text); 
+} 
+
 app.init = function() {
+
   app.variables(); 
-  
+
+  // Start It listener - scrolls to the Button Area
   $(`header button`).on(`click`, function() {
-    // scrolls to the Button Area
     app.scrollToElem(`main`);
   });
 
-  $(`.test button`).on(`click`, function() {
-    // tests something   
 
-    app.$preview.empty().append(
-      `<button type="button"> Test </buton>`
-    )
-
-
-  
+  // Button Text Input listener 
+  $(`.option.text input`).on(`keyup`, function() {
+      app.replaceButtonText($(this).val());
   });
+
+
+
+
+
+
+
+
+
+  // ----------------TEST BUTTON -----------------------------
+  $(`.test button`).on(`click`, function() {
+  
+    app.$button.empty().append(
+      `<button type="button"> Test </buton>`
+    )  
+
+  });
+
+
+
+
 }; // end of init
 
 $(() => {

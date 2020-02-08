e62f52fd788732ff52c08228008ffd1b85db8246
Ania-M-Pienio <aniapienio@gmail.com>
2020-02-07 21:47:08 -0500

    display functionality basic

--- index.js
+++ index.js
@@ -2,13 +2,29 @@ const app = {};
 
 // declares variables after doc is ready
 app.variables = function() {
-  // stores the preview button as jQuery object
+  // stores the preview button node as jQuery object
   app.$button = $(`.preview.edit button`);
 
-  // stores the current state of the preview button text 
-  app.buttonText = `button text`;
+  // object that  - set to default
+  app.button = {};
 
-}
+  // stores the current state of the preview button text - set to default
+  app.button.text = `button text`;
+
+  // store the current state of  the button's css
+  app.button.width = `200px`;
+  app.button.height = `100px`;
+  app.button.border = `none`;
+  app.button.backgroundColor = `#EDEAE5`;
+  app.button.color = `#FEF9C7`;
+  app.button.textDecoration = `none`;
+  app.button.fontFamily = `'Arial', sans-serif`;
+  app.button.fontSize = `16px`;
+  app.button.fontWeight = `bold`;
+  app.button.letterSpacing = `1.2`;
+  app.button.lineHeight = `normal`;
+  app.button.textAlign = `center`;
+}; // end of variables
 
 // scrolls to the element with the given id
 app.scrollToElem = function(id) {
@@ -18,30 +34,45 @@ app.scrollToElem = function(id) {
 
 // replaces the innter text node of the button
 app.setButtonText = function(inputText) {
-    // update the variable that stores the button text
-    app.buttonText = inputText;    
+  // update the variable that stores the button text
+  app.button.text = inputText;
 
-    // update the UI with the button text
-    app.$button.text(app.buttonText); 
-} 
+  // update the UI with the button text
+  app.$button.text(app.button.text);
+};
+
+// scrubs out old css (incl defaults) and replaces with all new css
+app.updateDisplayButton = function() {
+  app.$button.removeClass(`default`); // removes any defaults
+  app.$button.css("all", "unset"); // start css from scratch - variables will keep track of state
+  app.$button.css({
+    width: `${app.button.width}`,
+    height: `${app.button.height}`,
+    border: `${app.button.border}`,
+    "background-color": `${app.button.backgroundColor}`,
+    color: `${app.button.color}`,
+    "text-decoration": `${app.button.textDecoration}`,
+    "font-family": `${app.button.fontFamily}`,
+    "font-size": `${app.button.fontSize}`,
+    "font-weight": `${app.button.fontWeight}`,
+    "letter-spacing": `${app.button.letterSpacing}`,
+    "line-height": `${app.button.lineHeight}`,
+    "text-align": `${app.button.textAlign}`
+  });
+}; // end of updateDisplayButton
 
 app.init = function() {
+  app.variables();
 
-  app.variables(); 
-
-  // Start It handler - scrolls to the Button Area
+  // Handler Menu Button - scrolls to the Button Area
   $(`header button`).on(`click`, function() {
     app.scrollToElem(`main`);
   });
 
-
-  // Button Text Input handler
-  $(`.option.text input`).on(`keyup`, function() {       
-      app.setButtonText($(this).val());
+  // Handler Button Text Input
+  $(`.option.text input`).on(`keyup`, function() {
+    app.setButtonText($(this).val());
   });
-
-
-
 
 
 
@@ -49,13 +80,11 @@ app.init = function() {
 
 
   // ----------------TEST BUTTON -----------------------------
-  $(`.test button`).on(`click`, function() {
-  
-    app.$button.empty().append(
-      `<button type="button"> Test </buton>`
-    )  
+  $(`.test button`).on(`click`, function() {});
 
-  });
+  $(`.test2 button`).on(`click`, function() {});
+  // ----------------------------------------------------------
+
 
 
 

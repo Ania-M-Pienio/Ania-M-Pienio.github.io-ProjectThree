409dc08aca014ef5b2d65f8580b84fd9e889f370
Ania-M-Pienio <aniapienio@gmail.com>
2020-02-08 14:24:53 -0500

    update css code functionality

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ app.variables = function() {
 
   // stores the current state of the preview button text - set to default
   app.button.text = `button text`;
-
 }; // end of variables
 
 // set up the form with defaults
@@ -29,48 +28,51 @@ app.setup = function() {
   app.$button.text(app.button.text);
   app.$input.val(app.button.text);
   app.setDefaults();
-  app.setCSS();
+  app.updateCSS();
 };
 
 // store the current state of  the button's css
 app.setDefaults = function() {
   app.button = {
-    width:  { value: `200px`, color: `blue`},
-    height: { value: `100px`, color: `green`},
-    border: { value: `none` , color: `blue`},
-    color:  { value: `#FEF9C7`, color: `blue`},
-    background:       { value: `#D9D1C3`, color: `blue`},
-    "font-family":    { value: `'Arial', sans-serif`, color: `brown`},
-    "font-size":      { value: `16px`, color: `green`},
-    "font-weight":    { value: `bold`, color: `blue`},
-    "letter-spacing": { value: `1.2`, color: `green`},
-    "line-height":    { value: `normal`, color: `blue`},
-    "text-align":     { value: `center`, color: `blue`},
-  }
+    width: { value: `200px`, color: `green` },
+    height: { value: `100px`, color: `green` },
+    border: { value: `none`, color: `blue` },
+    color: { value: `#FEF9C7`, color: `blue` },
+    background: { value: `#D9D1C3`, color: `blue` },
+    "font-family": { value: `'Arial', sans-serif`, color: `brown` },
+    "font-size": { value: `16px`, color: `green` },
+    "font-weight": { value: `bold`, color: `blue` },
+    "letter-spacing": { value: `1.2`, color: `green` },
+    "line-height": { value: `normal`, color: `blue` },
+    "text-align": { value: `center`, color: `blue` }
+  };
 };
 
-app.setCSS = function() {
+// removes old listitems and adds new list-items into the code display
+app.updateCSS = function() {
   const $list = $(`ul`);
   $list.html(``);
   for (let cssItem in app.button) {
-    let listItem = this.getCSSHtml(cssItem, app.button[cssItem].value, app.button[cssItem].color);
-    $list.append(listItem);   
+    let listItem = this.getCSSHtml(
+      cssItem,
+      app.button[cssItem].value,
+      app.button[cssItem].color
+    );
+    $list.append(listItem);
   } // end of let-in
+}; // end of setCSS
 
-} // end of setCSS
-
-
-// creates a list item as css 
+// creates a list item as css and returns it
 app.getCSSHtml = function(key, value, color) {
   return `
     <li class="cssFormat"> 
       <span class="red"> ${key} </span>
       <span class="brown"> : </span>
       <span class="${color}"> ${value} </span>
-      <span class="brown">, </span>
+      <span class="brown">; </span>
     </li>   
     `;
-}
+};
 
 // scrolls to the element with the given id
 app.scrollToElem = function(id) {
@@ -78,16 +80,16 @@ app.scrollToElem = function(id) {
   element.scrollIntoView({ behavior: "smooth" });
 };
 
-// replaces the innter text node of the button
+// replaces the innter text node of the button with the given inputText
 app.updateText = function(inputText) {
   // update the variable that stores the button text
   app.button.text = inputText;
 
-  // update the UI with the button text
+  // update the UI with the stored button text
   app.$button.text(app.button.text);
 };
 
-// scrubs out old css (incl defaults) and replaces with all new css
+// scrubs out old css from the button and replaces with new css
 app.updateButton = function() {
   app.$button.removeClass(`default`); // removes any defaults
   app.$button.css("all", "unset"); // start css from scratch - variables will keep track of state
@@ -109,7 +111,6 @@ app.updateButton = function() {
 // jockeys control menu around depending on what is being opended/clicked
 app.toggleMenu = function($menu, $other) {
   // check if anything open - hide/show the menu as needed - so that the menu goes hidden on the next click
-
   app.menuOpen
     ? $menu.css({ display: `none` }) // close it
     : $menu.css({ display: `block` }); // open it
@@ -138,7 +139,6 @@ app.toggleMenu = function($menu, $other) {
 };
 
 app.init = function() {
- 
   app.setup();
 
   // Handler Banner Button - scrolls to the Button Area
@@ -158,12 +158,14 @@ app.init = function() {
     $(this).css({ background: `white` });
   });
 
+  // Handler for when mouse moves over a drop-down menu button
   app.$drops.on(`mouseenter`, function() {
     $(this)
       .parent()
       .css({ background: `#9FEDD7` });
   });
 
+  // Handler for when mouse moves from a drop-down menu button
   app.$drops.on(`mouseleave`, function() {
     app.menuOpen
       ? ""
@@ -189,19 +191,25 @@ app.init = function() {
     app.toggleMenu(app.$fillsMenu, app.$styles);
   });
 
+
+
   // ----------------TEST BUTTONS -----------------------------
+  // ----------------------------------------------------------
   $(`.test2 button`).on(`click`, function() {
-    app.button.backgroundColor = `#FCE181`;
-    app.button.border = `5px solid #E3430E`;
+    app.button.background.value = `#FCE181`;
+    app.button.border.value = `5px solid #E3430E`;
     app.updateButton();
+    app.updateCSS();
   });
 
   $(`.test button`).on(`click`, function() {
-    // $(`ul`).html(``);
-    const cssItem = app.getCSSHtml(`width`, `auto`, `blue`);
-    $(`ul`).append(cssItem);
+    app.updateCSS(); 
   });
+
   // ----------------------------------------------------------
+  // ----------------------------------------------------------
+
+
 }; // end of init
 
 $(() => {

ef90a9884ae05b20e3d18acbc65e2f879744b94a
Ania-M-Pienio <aniapienio@gmail.com>
2020-02-09 10:18:29 -0500

    organized setters and getters

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const app = {};
 
+/****************************************************************/
+/*****************        INITIALIZERS        *******************/
+/****************************************************************/
+
 // declares variables after doc is ready
 app.variables = function() {
   app.$button = $(`.preview.edit button`);
@@ -16,19 +20,84 @@ app.variables = function() {
   app.button.text = `button text`;
 }; // end of variables
 
-// set up the form with defaults
+// prepares defaults
 app.setup = function() {
   app.variables();
   app.$button.text(app.button.text);
   app.$input.val(app.button.text);
   app.setDefaults();
-  app.updateCSS();
-  app.updatePickers();
   $(`.code .display .preview .cssFormat`).css("transform", "scale(0.1)");
 };
 
+/****************************************************************/
+/*****************          SETTERS           *******************/
+/****************************************************************/
 
+// replaces the text that appears on the button
+app.updateText = function(inputText) {
+  // [1] updates the variable that stores the button text
+  app.button.text = inputText;
 
+  // [2] update the UI with the stored button text
+  app.$button.text(app.button.text);
+};
+
+// set the default state of the button's css
+app.setDefaults = function() {
+  app.button = {
+    width: { value: `200px`, color: `green` },
+    height: { value: `60px`, color: `green` },
+    border: { value: `4px solid #D9D1C3`, color: `blue` },
+    color: { value: `#026670`, color: `blue` },
+    background: { value: `#D9D1C3`, color: `blue` },
+    "font-family": { value: `'Arial', sans-serif`, color: `brown` },
+    "font-size": { value: `18px`, color: `green` },
+    "font-weight": { value: `bold`, color: `blue` },
+    "letter-spacing": { value: `1.2`, color: `green` },
+    "line-height": { value: `normal`, color: `blue` },
+    "text-align": { value: `center`, color: `blue` },
+    "border-radius": { value: `0`, color: `blue` },
+    "border-bottom": { value: `none`, color: `blue` }
+  };
+  app.updatePickers();
+  app.updateButton();
+  app.updateCSS();
+};
+
+// replaces old css with new css into button display
+app.updateButton = function() {
+  app.$button.css({
+    width: app.button.width.value,
+    height: app.button.height.value,
+    border: app.button.border.value,
+    color: app.button.color.value,
+    background: app.button.background.value,
+    "font-family": app.button["font-family"].value,
+    "font-size": app.button["font-size"].value,
+    "font-weight": app.button["font-weight"].value,
+    "letter-spacing": app.button["letter-spacing"].value,
+    "line-height": app.button["line-height"].value,
+    "text-align": app.button["text-align"].value,
+    "border-radius": app.button["border-radius"].value,
+    "border-bottom": app.button["border-bottom"].value
+  });
+}; // end of updateButton
+
+// removes old list items and adds new list-items into the code display
+app.updateCSS = function() {
+  const $list = $(`ul`);
+  $list.html(``);
+  for (let cssItem in app.button) {
+    let listItem = this.getCSSHtml(
+      cssItem,
+      app.button[cssItem].value,
+      app.button[cssItem].color
+    );
+    $list.append(listItem);
+  } // end of let-in
+}; // end of setCSS
+
+// updates the values and background color of the color pickers on fill menu
 app.updatePickers = function() {
   $(`input.jscolor:not(input#text):not(input#border)`).val(
     app.parseHex(app.button.background.value)
@@ -47,41 +116,11 @@ app.updatePickers = function() {
   });
 };
 
-// store the current state of  the button's css
-app.setDefaults = function() {
-  app.button = {
-    width: { value: `200px`, color: `green` },
-    height: { value: `60px`, color: `green` },
-    border: { value: `4px solid #D9D1C3`, color: `blue` },
-    color: { value: `#026670`, color: `blue` },
-    background: { value: `#D9D1C3`, color: `blue` },
-    "font-family": { value: `'Arial', sans-serif`, color: `brown` },
-    "font-size": { value: `18px`, color: `green` },
-    "font-weight": { value: `bold`, color: `blue` },
-    "letter-spacing": { value: `1.2`, color: `green` },
-    "line-height": { value: `normal`, color: `blue` },
-    "text-align": { value: `center`, color: `blue` },
-    "border-radius": { value: `0`, color: `blue` },
-    "border-bottom": { value: `none`, color: `blue` }
-  };
-};
+/****************************************************************/
+/*****************          GETTERS          *******************/
+/****************************************************************/
 
-// removes old listitems and adds new list-items into the code display
-app.updateCSS = function() {
-  const $list = $(`ul`);
-  $list.html(``);
-  for (let cssItem in app.button) {
-    let listItem = this.getCSSHtml(
-      cssItem,
-      app.button[cssItem].value,
-      app.button[cssItem].color
-    );
-    $list.append(listItem);
-  } // end of let-in
-}; // end of setCSS
-
-
-// creates a list item as css and returns it
+// creates a list item as html and returns it
 app.getCSSHtml = function(key, value, color) {
   return `
     <li class="cssFormat"> 
@@ -93,40 +132,9 @@ app.getCSSHtml = function(key, value, color) {
     `;
 };
 
-
-// scrolls to the element with the given id
-app.scrollToElem = function(id) {
-  let element = document.getElementById(id);
-  element.scrollIntoView({ behavior: "smooth" });
-};
-
-// replaces the innter text node of the button with the given
-app.updateText = function(inputText) {
-  // update the variable that stores the button text
-  app.button.text = inputText;
-
-  // update the UI with the stored button text
-  app.$button.text(app.button.text);
-};
-
-// scrubs out old css from the button and replaces with new css
-app.updateButton = function() {
-  app.$button.css({
-    width: app.button.width.value,
-    height: app.button.height.value,
-    border: app.button.border.value,
-    color: app.button.color.value,
-    background: app.button.background.value,
-    "font-family": app.button["font-family"].value,
-    "font-size": app.button["font-size"].value,
-    "font-weight": app.button["font-weight"].value,
-    "letter-spacing": app.button["letter-spacing"].value,
-    "line-height": app.button["line-height"].value,
-    "text-align": app.button["text-align"].value,
-    "border-radius": app.button["border-radius"].value,
-    "border-bottom": app.button["border-bottom"].value
-  });
-}; // end of updateDisplayButton
+/****************************************************************/
+/*****************           HELPERS          *******************/
+/****************************************************************/
 
 // jockeys control menu around depending on what is being opended/clicked
 app.toggleMenu = function($menu, $other) {
@@ -155,6 +163,13 @@ app.toggleMenu = function($menu, $other) {
     : $menu.parent().css({ background: `white` });
 };
 
+// scrolls to the element with the given id
+app.scrollToElem = function(id) {
+  let element = document.getElementById(id);
+  element.scrollIntoView({ behavior: "smooth" });
+};
+
+// removes the '#' from a color hex
 app.parseHex = function(hex) {
   if (hex.charAt(0) === `#`) {
     return hex.substring(1, hex.length);
@@ -163,42 +178,38 @@ app.parseHex = function(hex) {
   }
 };
 
+// extracts the color hex without the '#' from border css
 app.parseBorder = function(border) {
   return border.split(`#`)[1];
 };
 
-// All event handlers for Style
+// All event handlers for Style -------------------------------------
 app.launchStyles = function() {
-
-   // Hanlder Style Menu
+  // Hanlder Style Menu
   app.$styles.on(`click`, function() {
     app.toggleMenu(app.$stylesMenu, app.$fills);
   });
 
-
-    // Handler for resetting the styling
+  // Handler for resetting the styling
   $(`button.title`).on(`click`, function() {
     app.setDefaults();
-    app.updatePickers();
-    app.updateButton();
-    app.updateCSS();
   });
 
-  // Handle on change Style: Square 
+  // Handle on change Style: Square
   $(`button.square`).on(`click`, function() {
     app.button["border-radius"].value = `0`;
     app.updateButton();
     app.updateCSS();
   });
 
-  // Handle on change Style: Chip 
+  // Handle on change Style: Chip
   $(`button.chip`).on(`click`, function() {
     app.button["border-radius"].value = `30px`;
     app.updateButton();
     app.updateCSS();
   });
 
-  // Handle on change Style: Outline 
+  // Handle on change Style: Outline
   $(`button.outline`).on(`click`, function() {
     app.button.background.value = `#FFFFFF`;
     app.button.border.value = `4px solid ${app.button.color.value}`;
@@ -225,13 +236,11 @@ app.launchStyles = function() {
     app.updateButton();
     app.updateCSS();
   });
+};
 
-}
-
-// All event handlers for Fill
-app.launchFills = function () {
-
-    // Handler Fill Menu
+// All event handlers for Fill ------------------------------------
+app.launchFills = function() {
+  // Handler Fill Menu
   app.$fills.on(`click`, function() {
     app.toggleMenu(app.$fillsMenu, app.$styles);
   });
@@ -267,33 +276,41 @@ app.launchFills = function () {
     app.updateButton();
     app.updateCSS();
   });
-}
+};
 
-
+// All event handlers for viewing the css code ---------------------
 app.launchCode = function() {
-  
-}
+  // Handler for switching to css code
+  $(`button.switch`).on(`click`, function() {
+    $(`form.code`).css({
+      "margin-left": `0`
+    });
+    $(`form.edit .display.icons`).css({
+      visibility: `hidden`
+    });
+    $(`.code .display .preview .cssFormat`).css("transform", "scale(0.95)");
+  });
+
+  // Handler for switching back to button view mode
+  $(`button.view`).on(`click`, function() {
+    $(`form.code`).css({
+      "margin-left": "-2000px"
+    });
+    $(`form.edit .display.icons`).css({
+      visibility: `visible`
+    });
+  });
+};
 
 app.init = function() {
   app.setup();
   app.launchStyles();
   app.launchFills();
+  app.launchCode();
 
   // Handler Banner Button - scrolls to the Button Area
   $(`header button`).on(`click`, function() {
     app.scrollToElem(`main`);
-  });
-
-  // Handler for switching to css code
-  $(`button.switch`).on(`click`, function() {
-    $(`form.code`).css({
-      "margin-left": `0`,
-    });
-    $(`form.edit .display.icons`).css({
-      visibility: `hidden`
-    });
-    $(`.code .display .preview .cssFormat`)
-    .css("transform", "scale(0.95)");
   });
 
   // Handler Button Text Input Focus
@@ -327,16 +344,6 @@ app.init = function() {
   // Handler Apply Text Input
   app.$input.on(`keyup`, function() {
     app.updateText($(this).val());
-  });
-  
-  // Handler for switching back to button view mode
-  $(`button.view`).on(`click`, function() {
-    $(`form.code`).css({
-      "margin-left": "-2000px"
-    });
-    $(`form.edit .display.icons`).css({
-      visibility: `visible`
-    });
   });
 
   // ----------------TEST BUTTONS -----------------------------
